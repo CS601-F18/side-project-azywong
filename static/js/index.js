e9c78156a2ec82d01d7c37c753af55a2b6dd0d47
azywong <azywong@gmail.com>
2018-12-09 20:52:36 -0800

    * fixed front end javascript * got rid of unnecessary console logs * adding a new todo then toggling it saves as intended

--- static/js/index.js
+++ static/js/index.js
@@ -9,20 +9,19 @@ $(document).ready(function() {
     var text = $(".todo-form input[type=text]").val().trim();
     if (text.length > 0) {
       //append todo
-      $(".todo-list").append("<div class='todo-item'><input type='checkbox'>" + text + "<button class='delete'><i class='fas fa-trash-alt'></i></button></div>")
       $.ajax({
 			method: "POST",
 			url: "/todo",
 			data: { todo: text }
 		})
-		.done(function( msg ) {
-			console.log(msg);
+		.done(function (result) {
+      		$(".todo-list").append("<div class='todo-item' todoid='" + result.message.id + "'><input type='checkbox'>" + text + "<button class='delete'><i class='fas fa-trash-alt'></i></button></div>")
+
+		    $(".todo-item input[type=checkbox]").change(toggleTodo)
+		    $(".todo-item button").click(deleteTodo)
 		});
     }
 
-    $(".todo-item input[type=checkbox]").change(toggleTodo)
-
-    $(".todo-item button").click(deleteTodo)
 
 
     //clear input
@@ -32,51 +31,50 @@ $(document).ready(function() {
 
 // method that sends a post request to delete an event
 function deleteEvent() {
-	$(this).parent().remove();
+	var event = $(this).parent();
 	$.ajax({
 			method: "DELETE",
 			url: "/event",
 			data: { id: $(this).parent().attr("id") }
 		})
-		.done(function( msg ) {
-			console.log(msg);
+		.done(function() {
+			event.remove();
 		});
 }
 
 // method that sends a post request to delete a todo
 function deleteTodo () {
-    $(this).parent().remove();
+	var todo = $(this).parent()
     $.ajax({
 			method: "DELETE",
 			url: "/todo",
 			data: { id: $(this).parent().attr("todoId") }
 		})
 		.done(function( msg ) {
-			console.log(msg);
+			todo.remove();
 		});
 }
 
 // method that sends a post request to toggle the status of a todo
 function toggleTodo () {
+	var todo = $(this).parent();
 	if(this.checked) {
-		$(this).parent().addClass("inactive");
 		$.ajax({
 			method: "PUT",
 			url: "/todo",
 			data: { id: $(this).parent().attr("todoId"), checked: true }
 		})
 		.done(function( msg ) {
-			console.log(msg);
+			todo.addClass("inactive");
 		});
 	} else {
-		$(this).parent().removeClass("inactive");
 		$.ajax({
 			method: "PUT",
 			url: "/todo",
 			data: { id: $(this).parent().attr("todoId"), checked: false }
 		})
 		.done(function( msg ) {
-			console.log(msg);
+			todo.removeClass("inactive");
 		});
 	}
 }
